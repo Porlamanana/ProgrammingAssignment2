6bcb03725e9071e954966e690302c62ef3ae73e9
Cesar Alvarez <ceallas@gmail.com>
2014-09-18 13:19:09 +0200

    Commit Message

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        w <- NULL
+        s <- NULL
         set <- function(y) {
                 x <<- y
                 s <<- NULL
